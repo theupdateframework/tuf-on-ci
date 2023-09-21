a8745507c1fce8319ef4ae11c94a4c81dfa986ab
Jussi Kukkonen <jkukkonen@google.com>
2023-09-21 11:57:54 +0300

    Remove the bump-online command
    This will be handled in online-sign

--- repo/tuf_on_ci/__init__.py
+++ repo/tuf_on_ci/__init__.py
@@ -1,5 +1,5 @@
 from tuf_on_ci.build_repository import build_repository
-from tuf_on_ci.bump_expiring import bump_offline, bump_online
+from tuf_on_ci.bump_expiring import bump_offline
 from tuf_on_ci.online_sign import online_sign
 from tuf_on_ci.snapshot import snapshot
 from tuf_on_ci.status import status
--- repo/tuf_on_ci/bump_expiring.py
+++ repo/tuf_on_ci/bump_expiring.py
@@ -4,7 +4,6 @@
 
 import logging
 import subprocess
-import sys
 from glob import glob
 
 import click
@@ -25,57 +24,6 @@ def _git(cmd: list[str]) -> subprocess.CompletedProcess:
     proc = subprocess.run(cmd, check=True, capture_output=True, text=True)
     logger.debug("%s:\n%s", cmd, proc.stdout)
     return proc
-
-
-@click.command()  # type: ignore[arg-type]
-@click.option("-v", "--verbose", count=True, default=0)
-@click.option("--push/--no-push", default=False)
-@click.option("--metadata", required=True)
-@click.option("--targets", required=True)
-@click.argument("publish-dir", required=False)
-def bump_online(
-    verbose: int, push: bool, metadata: str, targets: str, publish_dir: str | None
-) -> None:
-    """Commit new metadata versions for online roles if needed
-
-    New versions will be signed.
-    If --push, then current branch is also pushed to origin
-    If publish-dir is provided, a repository snapshot is generated into that directory
-
-    returns 1 if new metadata was not generated
-    """
-    logging.basicConfig(level=logging.WARNING - verbose * 10)
-
-    msg = "Periodic online role version bump and resign\n\n"
-    repo = CIRepository("metadata")
-    snapshot_version = repo.bump_expiring("snapshot")
-    if snapshot_version is None:
-        timestamp_version = repo.bump_expiring("timestamp")
-        if timestamp_version is not None:
-            msg += f"timestamp v{timestamp_version}."
-    else:
-        # if snapshot changes, we need to actually update timestamp content
-        _, meta = repo.do_timestamp()
-        assert meta
-        timestamp_version = repo.timestamp().version
-        msg += f"snapshot v{snapshot_version}, timestamp v{timestamp_version}."
-
-    if not timestamp_version and not snapshot_version:
-        click.echo("No online version bumps needed")
-        sys.exit(1)
-
-    click.echo(msg)
-    _git(
-        ["commit", "-m", msg, "--", "metadata/timestamp.json", "metadata/snapshot.json"]
-    )
-    if push:
-        _git(["push", "origin", "HEAD"])
-
-    if publish_dir:
-        repo.publish(publish_dir, metadata, targets)
-        click.echo(f"New repository snapshot generated and published in {publish_dir}")
-    else:
-        click.echo("New repository snapshot generated")
 
 
 @click.command()  # type: ignore[arg-type]
