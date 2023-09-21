87517ddbabbda79f5501efe9ae98caa4450ab231
Jussi Kukkonen <jkukkonen@google.com>
2023-09-21 12:00:25 +0300

    repo: Remove snapshot command
    This is no longer used by the actions

--- repo/tuf_on_ci/__init__.py
+++ repo/tuf_on_ci/__init__.py
@@ -1,7 +1,6 @@
 from tuf_on_ci.build_repository import build_repository
 from tuf_on_ci.bump_expiring import bump_offline
 from tuf_on_ci.online_sign import online_sign
-from tuf_on_ci.snapshot import snapshot
 from tuf_on_ci.status import status
 
 __all__ = ["build_repository", "bump_offline", "bump_online", "online_sign", "snapshot", "status"]
