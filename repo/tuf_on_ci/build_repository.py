453945d8bc2d9a6cd7bd98ed46c10d45d3120f09
Jussi Kukkonen <jkukkonen@google.com>
2023-09-22 14:06:44 +0300

    repo: minor lint fixes

--- repo/tuf_on_ci/build_repository.py
+++ repo/tuf_on_ci/build_repository.py
@@ -3,6 +3,7 @@
 """Command line tool to build a publishable TUF-on-CI repository"""
 
 import logging
+
 import click
 
 from tuf_on_ci._repository import CIRepository
@@ -12,11 +13,9 @@ logger = logging.getLogger(__name__)
 
 @click.command()  # type: ignore[arg-type]
 @click.option("-v", "--verbose", count=True, default=0)
-@click.option("--metadata", required = True)
+@click.option("--metadata", required=True)
 @click.option("--artifacts")
-def build_repository(
-    verbose: int, metadata: str, artifacts: str | None
-) -> None:
+def build_repository(verbose: int, metadata: str, artifacts: str | None) -> None:
     """Create publishable metadata and artifact directories"""
     logging.basicConfig(level=logging.WARNING - verbose * 10)
 
