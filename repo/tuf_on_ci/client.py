9d5dd3b3b1e84f82bf399f3a3ffc60c47358c4b5
Jussi Kukkonen <jkukkonen@google.com>
2024-03-30 11:48:03 +0200

    actions: test-repository API change
    * remove --metadata-cache from test-client CLI: it was not used
    * Add --compare-source flag: this decides if we want to compare
      the clients metadata to files in current source directory
      * default is on: this is what tuf-on-ci should keep using
      * when running outside of a tuf-on-ci repo, --no-compare-source
        may be a better choice
    * Also start initializing the repository with 1.root.json from the
      metadata URL: this way --no-compare-source needs no initial data

--- repo/tuf_on_ci/client.py
+++ repo/tuf_on_ci/client.py
@@ -5,11 +5,11 @@
 
 import logging
 import os
-import shutil
 import sys
 from datetime import datetime
 from filecmp import cmp
 from tempfile import TemporaryDirectory
+from urllib import request
 
 import click
 from tuf.api.exceptions import ExpiredMetadataError
@@ -22,7 +22,7 @@ def expiry_check(dir: str, role: str, timestamp: int):
     md = Metadata.from_file(os.path.join(dir, f"{role}.json"))
     expiry = md.signed.expires
     if ref_time > expiry:
-        sys.exit(f"Error: {role} expires {expiry} (expected after {ref_time})")
+        sys.exit(f"Error: {role} expires {expiry} (expected valid at {ref_time})")
     print(f"Role {role} is valid on {ref_time}: OK")
 
 @click.command()
@@ -30,20 +30,15 @@ def expiry_check(dir: str, role: str, timestamp: int):
 @click.option("-m", "--metadata-url", type=str, required=True)
 @click.option("-a", "--artifact-url", type=str, required=True)
 @click.option("-e", "--expected-artifact", type=str)
-@click.option(
-    "-c",
-    "--metadata-cache",
-    type=str,
-    help="Optional client cache dir that client should use",
-)
+@click.option("--compare-source/--no-compare-source", default=True)
 @click.option("-t", "--time", type=int)
-@click.option("--offline-time", type=int)
+@click.option("-o", "--offline-time", type=int)
 def client(
     verbose: int,
     metadata_url: str,
     artifact_url: str,
     expected_artifact: str | None,
-    metadata_cache: str | None,
+    compare_source: bool,
     time: int | None,
     offline_time: int | None,
 ) -> None:
@@ -61,16 +56,11 @@ def client(
         os.mkdir(metadata_dir)
         os.mkdir(artifact_dir)
 
-        if metadata_cache:
-            # pre-populate the client cache with given directory
-            for fname in os.listdir(metadata_cache):
-                fpath = os.path.join(metadata_cache, fname)
-                if os.path.isfile(fpath):
-                    shutil.copy(fpath, os.path.join(metadata_dir, fname))
-        else:
-            # pre-populate the client cache with first root version from source
-            root = "metadata/root_history/1.root.json"
-            shutil.copy(root, os.path.join(metadata_dir, "root.json"))
+        root_url = f"{metadata_url}/1.root.json"
+        try:
+            request.urlretrieve(root_url, f"{metadata_dir}/root.json")  # noqa: S310
+        except OSError as e:
+            sys.exit(f"Failed to download initial root {root_url}: {e}")
 
         updater = Updater(metadata_dir, metadata_url, artifact_dir, artifact_url)
         ref_time_string = ""
@@ -80,20 +70,23 @@ def client(
             updater._trusted_set.reference_time = datetime.fromtimestamp(time)
             ref_time_string = f" (reference time {updater._trusted_set.reference_time})"
 
-        # For now, just confirm we can get top level metadata from remote
+        # Confirm we can get valid top level metadata from remote
         try:
             updater.refresh()
         except ExpiredMetadataError as e:
             sys.exit(f"Update{ref_time_string} failed: {e}")
         print(f"Client metadata update{ref_time_string}: OK")
 
-        # Verify the received metadata versions are what was expected
-        for f in ["root.json", "timestamp.json"]:
-            if not cmp(f"metadata/{f}", os.path.join(metadata_dir, f), shallow=False):
-                sys.exit(f"Error: Client metadata does not match sources: {f} failed")
-        print("Client metadata matches sources: OK")
+        if compare_source:
+            # Compare received metadata versions with source metadata
+            for f in ["root.json", "timestamp.json"]:
+                client_file = os.path.join(metadata_dir, f)
+                source_file = os.path.join("metadata", f)
+                if not cmp(source_file, client_file, shallow=False):
+                    sys.exit(f"Error: metadata does not match sources: {f} failed")
+            print("Client metadata matches sources: OK")
 
-        # Verify root and targets are valid at given reference times
+        # Verify root and targets are valid at given reference time
         if offline_time is not None:
             expiry_check(metadata_dir, "root", offline_time)
             expiry_check(metadata_dir, "targets", offline_time)
