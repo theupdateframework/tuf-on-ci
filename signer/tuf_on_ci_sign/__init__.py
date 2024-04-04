1345aaa89c5d0673f5f0ea811b9742a300a1fd00
Jussi Kukkonen <jkukkonen@google.com>
2024-04-04 13:15:30 +0300

    Release v0.9

--- signer/tuf_on_ci_sign/__init__.py
+++ signer/tuf_on_ci_sign/__init__.py
@@ -2,6 +2,6 @@ from tuf_on_ci_sign.delegate import delegate
 from tuf_on_ci_sign.import_repo import import_repo
 from tuf_on_ci_sign.sign import sign
 
-__version__ = "0.8.0"
+__version__ = "0.9.0"
 
 __all__ = ["delegate", "import_repo", "sign"]
