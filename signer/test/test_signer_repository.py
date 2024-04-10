6790d82bdfe166bf1d2b1ee60ae23f3d47cf08c0
Jussi Kukkonen <jkukkonen@google.com>
2024-04-10 16:51:07 +0300

    signer/tests: Avoid deprecated assertEquals

--- signer/test/test_signer_repository.py
+++ signer/test/test_signer_repository.py
@@ -1,13 +1,14 @@
 import unittest
 
-from tuf_on_ci_sign._signer_repository import build_paths, SignerRepository
+from tuf_on_ci_sign._signer_repository import SignerRepository, build_paths
+
 
 class TestUser(unittest.TestCase):
     """Test delegate path generation"""
 
     def test_build_paths(self):
        paths = build_paths("myrole", SignerRepository.MAX_DEPTH)
-       self.assertEquals(paths, ['myrole/*', 'myrole/*/*', 'myrole/*/*/*', 'myrole/*/*/*/*'])
+       self.assertEqual(paths, ['myrole/*', 'myrole/*/*', 'myrole/*/*/*', 'myrole/*/*/*/*'])
 
 if __name__ == "__main__":
     unittest.main()
