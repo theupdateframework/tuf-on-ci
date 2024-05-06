b4b0ebc1283b685930a7b19f1111789ba1a37771
Jussi Kukkonen <jkukkonen@google.com>
2024-05-06 15:21:28 +0300

    build-repository: Build a descriptive markdown file as well
    As a start this lists the signers for each role

--- repo/tuf_on_ci/build_repository.py
+++ repo/tuf_on_ci/build_repository.py
@@ -3,12 +3,46 @@
 """Command line tool to build a publishable TUF-on-CI repository"""
 
 import logging
+import os
 
 import click
+from tuf.api.metadata import Root, Targets
 
 from tuf_on_ci._repository import CIRepository
 
 logger = logging.getLogger(__name__)
+
+
+def build_description(repo: CIRepository) -> str:
+    lines = [
+        "## Signers",
+        "| Role | required # of signatures | Signers |",
+        "| - | - | - |",
+    ]
+    root = repo.root()
+    targets = repo.targets()
+    roles: list[tuple[Root | Targets, str]] = [
+        (root, "root"),
+        (root, "timestamp"),
+        (root, "snapshot"),
+        (root, "targets"),
+    ]
+    if targets.delegations and targets.delegations.roles:
+        for rolename in targets.delegations.roles:
+            roles.append((targets, rolename))
+
+    for delegator, rolename in roles:
+        role = delegator.get_delegated_role(rolename)
+        keys = [delegator.get_key(keyid) for keyid in role.keyids]
+        signers = []
+        for key in keys:
+            if "x-tuf-on-ci-keyowner" in key.unrecognized_fields:
+                signers.append(key.unrecognized_fields["x-tuf-on-ci-keyowner"])
+            else:
+                signers.append("_online key_")
+        lines.append(f"| {rolename} | {role.threshold} | {', '.join(signers)} |")
+
+    return "\n".join(lines)
 
 
 @click.command()  # type: ignore[arg-type]
@@ -21,6 +55,10 @@ def build_repository(verbose: int, metadata: str, artifacts: str | None) -> None
 
     repo = CIRepository("metadata")
     repo.build(metadata, artifacts)
+
     click.echo(f"Metadata published in {metadata}")
     if artifacts:
         click.echo(f"Artifacts published in {artifacts}")
+
+    with open(os.path.join(metadata, "index.md"), "w") as f:
+        f.write(build_description(repo))
