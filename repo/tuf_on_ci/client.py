b3498cfecb264f86afd4738a5659facaa33758ca
Fredrik Skogman <kommendorkapten@github.com>
2024-07-15 11:01:31 +0200

    ruff format fixes
    Signed-off-by: Fredrik Skogman <kommendorkapten@github.com>

--- repo/tuf_on_ci/client.py
+++ repo/tuf_on_ci/client.py
@@ -77,11 +77,9 @@ def client(
 
         if update_base_url is not None:
             # Update client to update_base_url before doing the actual update
-            updater = Updater(metadata_dir,
-                              update_base_url,
-                              artifact_dir,
-                              artifact_url,
-                              config=config)
+            updater = Updater(
+                metadata_dir, update_base_url, artifact_dir, artifact_url, config=config
+            )
             try:
                 updater.refresh()
                 print(f"Client metadata update from base url {update_base_url}: OK")
@@ -89,11 +87,9 @@ def client(
                 print(f"WARNING: update base url has expired metadata: {e}")
 
         # Update client to metadata_url
-        updater = Updater(metadata_dir,
-                          metadata_url,
-                          artifact_dir,
-                          artifact_url,
-                          config=config)
+        updater = Updater(
+            metadata_dir, metadata_url, artifact_dir, artifact_url, config=config
+        )
         ref_time_string = ""
         if time is not None:
             # HACK: replace reference time with ours: initial root has been loaded
