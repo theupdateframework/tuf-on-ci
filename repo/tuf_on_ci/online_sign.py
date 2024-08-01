8ad13d1bbcbf768d0aa0b52511743ccc2bdb8f80
Jussi Kukkonen <jkukkonen@google.com>
2024-08-01 12:59:52 +0300

    actions: Add initial online targets signing into signing-event
    * this also adds a separate tuf-on-ci-online-sign-targets command
    * tuf-on-ci-online-sign is reverted so it only manages
      timestamp/snapshot

--- repo/tuf_on_ci/online_sign.py
+++ repo/tuf_on_ci/online_sign.py
@@ -29,13 +29,10 @@ def _git(cmd: list[str]) -> subprocess.CompletedProcess:
 @click.command()  # type: ignore[arg-type]
 @click.option("-v", "--verbose", count=True, default=0)
 @click.option("--push/--no-push", default=False)
-@click.option("--role", default="")
-def online_sign(verbose: int, push: bool, role: str) -> None:
+def online_sign(verbose: int, push: bool) -> None:
     """Update The TUF snapshot and timestamp if needed
 
-    If role is provided, that role is updated instead.
-
-    Create a commit with the updated role changes (if any).
+    Create a commit with the snapshot and timestamp changes (if any).
     If --push, the commit is pushed to origin.
 
     A new snapshot will be created if
@@ -47,38 +44,21 @@ def online_sign(verbose: int, push: bool, role: str) -> None:
     * A new snapshot was created
     * or timestamp is in signing period
     * or timestamp is currently not correctly signed
-
-    Provided role will be updated if
-    * Content is changed
-    * or role is in signing period
-    * or role is currently not correctly signed
     """
 
     logging.basicConfig(level=logging.WARNING - verbose * 10)
     repo = CIRepository("metadata")
-    signed = False
+    valid_snapshot = repo.is_signed("snapshot")
+    snapshot_updated, _ = repo.do_snapshot(not valid_snapshot)
+    valid_timestamp = repo.is_signed("timestamp")
+    timestamp_updated, _ = repo.do_timestamp(not valid_timestamp)
 
-    if role != "":
-        repo.sign(role)
-        # Exception is raised if nothing is signed
-        signed = True
-        roles = role
-        files = [repo._get_filename(role)]
-    else:
-        valid_snapshot = repo.is_signed("snapshot")
-        snapshot_updated, _ = repo.do_snapshot(not valid_snapshot)
-        valid_timestamp = repo.is_signed("timestamp")
-        timestamp_updated, _ = repo.do_timestamp(not valid_timestamp)
+    if timestamp_updated:
         roles = "snapshot & timestamp" if snapshot_updated else "timestamp"
-        signed = timestamp_updated
-        files = ["metadata/timestamp.json", "metadata/snapshot.json"]
-    if signed:
         msg = f"Online sign ({roles})"
 
         click.echo(msg)
-        cmd = ["add"]
-        cmd.extend(files)
-        _git(cmd)
+        _git(["add", "metadata/timestamp.json", "metadata/snapshot.json"])
         _git(["commit", "-m", msg, "--signoff"])
         if push:
             _git(["push", "origin", "HEAD"])
