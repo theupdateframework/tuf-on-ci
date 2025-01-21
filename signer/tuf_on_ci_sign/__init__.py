8ab0d233258768fd43cb1fe57c57629305b42ddb
Fredrik Skogman <kommendorkapten@github.com>
2025-01-21 08:54:29 +0100

    Prepare for release v0.15.2
    Signed-off-by: Fredrik Skogman <kommendorkapten@github.com>

--- signer/tuf_on_ci_sign/__init__.py
+++ signer/tuf_on_ci_sign/__init__.py
@@ -2,6 +2,6 @@ from tuf_on_ci_sign.delegate import delegate
 from tuf_on_ci_sign.import_repo import import_repo
 from tuf_on_ci_sign.sign import sign
 
-__version__ = "0.15.1"
+__version__ = "0.15.2"
 
 __all__ = ["delegate", "import_repo", "sign"]
